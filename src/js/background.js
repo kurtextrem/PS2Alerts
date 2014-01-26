a4f73fbc5d2f2e9369296fda885bf3cb04b8e032
Jacob Groß <kurtextrem@gmail.com>
2014-01-26 09:31:58 +0100

    changed orders, better code overview

--- src/js/background.js
+++ src/js/background.js
@@ -1,10 +1,14 @@
-+function(window) {
++function (window) {
 	'use strict';
 
 	var servers = [
 		//{name: 'Briggs', id: 25, status: 0},
 		//{name: 'Ceres', id: 11, status: 0},
-		{name: 'Cobalt', id: 13, status: 0},
+		{
+			name: 'Cobalt',
+			id: 13,
+			status: 0
+		},
 		//{name: 'Connery', id: 1, status: 0},
 		//{name: 'Mattherson', id: 17, status: 0},
 		//{name: 'Miller', id: 10, status: 0},
@@ -29,20 +33,6 @@
 		{zone: 8, type: 4}
 	]
 
-	var zoneData = {
-		0: 'Global',
-		2: 'Indar',
-		6: 'Amerish',
-		8: 'Esamir'
-	}
-
-	var typeData = {
-		1: 'Territory',
-		2: 'Bio Labs',
-		3: 'Tech Plants',
-		4: 'Amp Stations'
-	}
-
 	var activeEvent = {
 		135: true,
 		136: true,
@@ -57,9 +47,10 @@
 	}
 
 
-	var Alert = function() {
+	var Alert = function () {
 		this.init()
 	}
+
 	Alert.prototype = {
 		url: 'http://census.soe.com/get/ps2:v2/',
 
@@ -72,16 +63,27 @@
 		remember: false,
 		count: 0,
 
-		init: function(force) {
-			chrome.storage.local.get({main: 13, flare: 0, lastUpdate: 0, servers: '', notification: 13, hide: 0, count: 0}, function(data) {
-				if (data.servers === '') {
+		init: function (force) {
+			chrome.storage.local.get({
+				main: 13,
+				flare: 0,
+				lastUpdate: 0,
+				servers: {},
+				notification: 13,
+				hide: 0,
+				count: 0,
+				remember: false
+			}, function (data) {
+				if (typeof data.servers[0] === 'undefined') {
 					var serverObj = {}
-					$.each(servers, function(i, server) {
-						serverObj['s'+server.id] = server
-						serverObj['s'+server.id].alert = {}
+					$.each(servers, function (i, server) {
+						serverObj['s' + server.id] = server
+						serverObj['s' + server.id].alert = {}
 					})
 					this.servers = data.servers = serverObj
-					chrome.storage.local.set({servers: data.servers})
+					chrome.storage.local.set({
+						servers: data.servers
+					})
 				} else {
 					this.servers = data.servers
 				}
@@ -90,31 +92,25 @@
 				this.notification = data.notification
 				this.hide = data.hide
 				this.count = data.count
-				var main = data.servers['s'+this.main]
-				if (force || $.now()-data.lastUpdate > 300000) {
+				this.remember = data.remember
+
+				var main = this.servers.servers['s' + this.main]
+				if (force || $.now() - data.lastUpdate > 300000) {
 					this.update()
 				} else {
 					this.setBadgeAlarm(main)
 				}
-				this.registerAlarms()
+				this.registerUpdateAlarms()
 			}.bind(this))
 		},
 
-		sendToPopup: function(server) {
-			chrome.storage.local.get({servers: {}}, function(data) {
-				data.servers['s'+server.id] = server
-				this.servers['s'+server.id] = server
-				chrome.storage.local.set({servers: data.servers})
-				var popups = chrome.extension.getViews({type: 'popup'})
-				if (0 < popups.length)
-					popups[0].app.updated(server)
-			}.bind(this))
-		},
-
-		update: function() {
-			chrome.storage.local.set({count: 0, lastUpdate: $.now()})
+		update: function () {
+			chrome.storage.local.set({
+				count: 0,
+				lastUpdate: $.now()
+			})
 			this.count = 0
-			servers.forEach(function (server) {
+			$.each(this.servers, function (index, server) {
 				$.ajax(this.url + 'world?world_id=' + server.id, {
 					dataType: 'json',
 					success: function (response) {
@@ -130,123 +126,117 @@
 							server.status = 'errorUA'
 							this.sendToPopup(server)
 						}
-					}.bind(this)
+					}.bind(this),
+					error: function () {
+						server.status = 'errorAPI'
+						this.sendToPopup(server)
+					}
 				})
 			}.bind(this))
 		},
 
-		_updateServer: function(server) {
-			$.ajax(this.url + 'world_event?world_id=' + server.id + '&type=METAGAME', {
-				dataType: 'json',
-				success: function (response) {
-					if (response && response.world_event_list) {
-						var data = response.world_event_list[0]
-						if (activeEvent[+data.metagame_event_state]) {
-							var event = events[+data.metagame_event_id - 1]
+		sendToPopup: function (server) {
+			chrome.storage.local.get({ servers: {} }, function (data) {
+				var id = 's' + server.id
+				this.servers[id] = data.servers[id] = server
+				chrome.storage.local.set({
+					servers: data.servers
+				})
 
-							server.status = 1
-							server.alert = {
-								start: +(data.timestamp + '000'),
-								type: typeData[event.type],
-								zone: zoneData[event.zone],
-								notified: false,
-								faction_nc: data.faction_nc,
-								faction_tr: data.faction_tr,
-								faction_vs: data.faction_vs,
-								experience_bonus: data.experience_bonus || 0
-							}
-
-							if (server.id === this.main) {
-								this.setBadgeAlarm(server)
-							} else {
-								chrome.browserAction.setBadgeText({text: ''})
-								chrome.alarms.clear('update-badge')
-								if (this.hide)
-									chrome.browserAction.disable()
-								this.updateIcon('img/notification_tray_empty.png')
-							}
-							if (server.id === this.notification || this.notification === 0) {
-								if (!this.servers['s'+server.id].alert.notified) {
-									this.createNotification(server)
-									server.alert.notified = true
-								} else {
-									server.alert.notified = true
-								}
-							}
-							this.count++
-							chrome.storage.local.set({count: this.count})
-						} else {
-							server.status = 'no alert'
-						}
-
-						this.sendToPopup(server)
-						return
-					} else {
-						server.status = 'errorUS'
-						this.sendToPopup(server)
-					}
-				}.bind(this)
-			})
-		},
-
-		registerAlarms: function() {
-			chrome.alarms.create('update', {delayInMinutes: 5, periodInMinutes: 5})
-			chrome.alarms.onAlarm.addListener(function(alarm) {
-				if (alarm.name === 'update')
-					this.update()
+				var popups = chrome.extension.getViews({ type: 'popup' })
+				if (0 < popups.length)
+					popups[0].app.updated(server)
 			}.bind(this))
 		},
 
-		setBadgeAlarm: function(server) {
+		_updateServer: function (server) {
+			$.ajax(this.url + 'world_event?world_id=' + server.id + '&type=METAGAME', {
+				dataType: 'json',
+				success: function (response) {
+					if (!response && !response.world_event_list) {
+						server.status = 'errorUS'
+						this.clearBadgeAlarm()
+						return this.sendToPopup(server)
+					}
+
+					var data = response.world_event_list[0]
+					if (!activeEvent[+data.metagame_event_state]) {
+						server.status = 'no alert'
+						this.clearBadgeAlarm()
+						return this.sendToPopup(server)
+					}
+
+					this.count++
+					chrome.storage.local.set({
+						count: this.count
+					})
+
+					var event = events[+data.metagame_event_id - 1]
+
+					server.status = 1
+					server.alert = {
+						start: +(data.timestamp + '000'),
+						type: event.type,
+						zone: event.zone,
+						notified: false,
+						faction_nc: data.faction_nc,
+						faction_tr: data.faction_tr,
+						faction_vs: data.faction_vs,
+						experience_bonus: data.experience_bonus || 0
+					}
+
+					if (server.id === this.main) {
+						this.setBadgeAlarm(server)
+					} else {
+						this.clearBadgeAlarm()
+					}
+
+					if (server.id === this.notification || this.notification === 0) {
+						if (this.servers['s' + server.id].alert && !this.servers['s' + server.id].alert.notified) {
+							server.alert.notified = true
+							this.createNotification(server)
+						} else {
+							server.alert.notified = true
+						}
+					}
+
+					this.sendToPopup(server)
+				}.bind(this),
+				error: function () {
+					server.state = 'errorAPI'
+					this.sendToPopup(server)
+				}
+			})
+		},
+
+		clearBadgeAlarm: function() {
+			chrome.browserAction.setBadgeText({ text: '' })
+			chrome.alarms.clear('update-badge')
+			this.updateIcon('img/notification_tray_empty.png')
+			if (this.hide)
+				chrome.browserAction.disable()
+		},
+
+		setBadgeAlarm: function (server) {
 			this._updateBadge(server)
-			chrome.alarms.create('update-badge', {delayInMinutes: 1, periodInMinutes: 1})
-			chrome.alarms.onAlarm.addListener(function(alarm) {
+			chrome.alarms.create('update-badge', {
+				delayInMinutes: 1,
+				periodInMinutes: 1
+			})
+			chrome.alarms.onAlarm.addListener(function (alarm) {
 				if (alarm.name === 'update-badge')
 					this._updateBadge(server)
 			}.bind(this))
 		},
 
-		createNotification: function(server, remember) {
-			var opt = {
-			  	type: 'basic',
-			  	iconUrl: 'img/AlertIconWaves3.png'
-			}
-			if (remember) {
-				opt.title = server.name+' Alert: 30min left!'
-			} else {
-				opt.title = server.name+': Alert just started!'
-				opt.message = 'An alert started on '+server.name+'.'
-				opt.buttons = [
-					{
-						title: 'Remeber 30min before alert end'
-					}
-				]
-			}
-			chrome.notifications.create(server.id+'-alert', opt, function(id) {
-				chrome.notifications.onButtonClicked.addListener(function(id, index){
-					this.remember = true
-				}.bind(this))
-				chrome.alarms.create('close-notification', {delayInMinutes: 1})
-				chrome.alarms.onAlarm.addListener(function(alarm) {
-					if (alarm.name === 'close-notification')
-						chrome.notifications.clear(id, function(){})
-				})
-			}.bind(this))
-		},
-
-		_updateBadge: function(server) {
-			if (server.status === 'no alert') {
-				this.updateIcon('img/notification_tray_empty.png')
-				chrome.browserAction.setBadgeText({text: ''})
-				chrome.alarms.clear('update-badge')
-				if (this.hide)
-					chrome.browserAction.disable()
-			}
+		_updateBadge: function (server) {
+			if (server.status === 'no alert')
+				this.clearBadgeAlarm()
 			if (server.status === 1) {
 				var current = Date.now(),
-				date = new Date(server.alert.start - current)
+					date = new Date(server.alert.start - current)
 
-				if (server.alert.type === 'Territory' || server.alert.zone === 'Global') {
+				if (server.alert.type === 1 || server.alert.zone === 0) {
 					date.setUTCHours(date.getUTCHours() + 2)
 				} else {
 					date.setUTCHours(date.getUTCHours() + 1)
@@ -256,47 +246,85 @@
 				var m = ('0' + date.getUTCMinutes()).slice(-2)
 
 				if (h > 2 || (h + +m) < 0) {
-					this.updateIcon('img/notification_tray_empty.png')
-					chrome.browserAction.setBadgeText({text: ''})
-					chrome.alarms.clear('update-badge')
-					if (this.hide)
-						chrome.browserAction.disable()
+					this.clearBadgeAlarm()
 				} else {
 					chrome.browserAction.enable()
 					if (this.remember && h < 1 && m <= 30) {
-						this.createNotification(server, true)
 						this.remember = false
+						chrome.storage.local.set({remember: false})
+						this.createNotification(server, true)
 					}
 					this.updateIcon('img/notification_tray_attention.png')
-					chrome.browserAction.setBadgeText({text: h + ':' + m})
-					chrome.browserAction.setBadgeBackgroundColor({color: flares[this.flare]})
+					chrome.browserAction.setBadgeText({
+						text: h + ':' + m
+					})
+					chrome.browserAction.setBadgeBackgroundColor({
+						color: flares[this.flare]
+					})
 				}
 			}
 		},
 
-		updateIcon: function(path) {
+		registerUpdateAlarms: function () {
+			chrome.alarms.create('update', { delayInMinutes: 3, periodInMinutes: 3 })
+			chrome.alarms.onAlarm.addListener(function (alarm) {
+				if (alarm.name === 'update')
+					this.update()
+			}.bind(this))
+		},
+
+		createNotification: function (server, remember) {
+			var opt = {
+				type: 'basic',
+				iconUrl: 'img/AlertIconWaves3.png'
+			}
+			if (remember) {
+				opt.title = server.name + ' Alert: 30min left!'
+				opt.message = '30mins left, start playing now!'
+			} else {
+				opt.title = server.name + ': Alert just started!'
+				opt.message = 'An alert started on ' + server.name + '.'
+				opt.buttons = [{
+					title: 'Remeber 30min before alert end'
+				}]
+			}
+			chrome.notifications.create(server.id + '-alert', opt, function (id) {
+				chrome.notifications.onButtonClicked.addListener(function (id, index) {
+					this.remember = true
+					chrome.storage.local.set({remember: true})
+				}.bind(this))
+				chrome.alarms.create('close-notification', { delayInMinutes: 1 })
+				chrome.alarms.onAlarm.addListener(function (alarm) {
+					if (alarm.name === 'close-notification')
+						chrome.notifications.clear(id, function () {})
+				})
+			}.bind(this))
+		},
+
+		updateIcon: function (path) {
 			var canvas = $('canvas')
 			if (canvas.length < 1) {
 				canvas = $('<canvas width="19" height="19"></canvas>')
-				$('body').append(canvas)
+				$('body')
+					.append(canvas)
 			}
 			var context = canvas[0].getContext('2d'),
-			imageObj = new Image()
+				imageObj = new Image()
 
-			imageObj.onload = function() {
-				context.clearRect(0, 0, 19, 19)
-				context.drawImage(imageObj, 0, 0, 19, 19)
-				context.fillStyle = '#888'
-				context.fillText(this.count, 6.5, 12)
-				var details = { imageData: 0 }
-				details.imageData = context.getImageData(0, 0, 19, 19)
-				chrome.browserAction.setIcon(details)
-			}.bind(this)
-			imageObj.src = path
+				imageObj.onload = function () {
+					context.clearRect(0, 0, 19, 19)
+					context.drawImage(imageObj, 0, 0, 19, 19)
+					context.fillStyle = '#888'
+					context.fillText(this.count, 6.5, 12)
+					var details = {
+						imageData: 0
+					}
+					details.imageData = context.getImageData(0, 0, 19, 19)
+					chrome.browserAction.setIcon(details)
+				}.bind(this)
+				imageObj.src = path
 		}
 	}
 
-	window.Alert = Alert
+	window.alert = new Alert()
 }(window)
-
-alert = new Alert()
--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,35 @@
 +function(window) {
 	'use strict';
 
+	var zoneData = {
+		0: 'Global',
+		2: 'Indar',
+		6: 'Amerish',
+		8: 'Esamir'
+	}
+
+	var typeData = {
+		1: 'Territory',
+		2: 'Bio Labs',
+		3: 'Tech Plants',
+		4: 'Amp Stations'
+	}
+
 	var App = function() {
-		chrome.storage.local.get({servers: {}, main: 13, lastUpdate: 0}, function(data) {
+		chrome.storage.local.get({servers: {}, main: 13, lastUpdate: 0, direction: 'desc', by: 'server'}, function(data) {
 			this.servers = data.servers
 			this.addHTML()
 			$.each(data.servers, function(index, server) {
-				this.updateAlerts(server)
+				this.updateTable(server)
 			}.bind(this))
+			this.interval = window.setInterval(this.updateTime.bind(this), 1000)
+
+			var $table = $('table')
+			$table.tablesort()
+			$table.data('tablesort').sort($('#'+data.by), data.direction)
+
 			$('#refresh').click(this.refresh)
-			chrome.storage.local.get({direction: 'desc', by: 'server'}, function(data) {
-				var $table = $('table')
-				$table.tablesort()
-				$table.data('tablesort').sort($('#'+data.by), data.direction)
-			})
-			$('table').on('tablesort:complete', function(e, tablesort) {
+			$table.on('tablesort:complete', function(e, tablesort) {
 				chrome.storage.local.set({direction: tablesort.direction, by: $('.sorted').attr('id')})
 			})
 			$('#server-'+data.main+' td').css('background-color', '#d0e9c6')
@@ -23,10 +38,89 @@
 	}
 	App.prototype = {
 		servers: {},
-		intervals: {},
+		interval: null,
 		constructor: App,
+
+		addHTML: function() {
+			$.each(this.servers, function (index, server) {
+				$('tbody').append('<tr id="server-' + server.id + '"></tr>')
+				$('tr:last').append('<td class="server-name"><button type="button" data-toggle="collapse" data-target="#collapse'+server.id+'" class="btn btn-link">' + server.name + '</button></td>')
+				$('tr:last').append('<td class="remaining"></td>')
+				$('tr:last').append('<td class="type"></td>')
+				$('tr:last').append('<td class="continent"></td>')
+			})
+		},
+
+		updateTable: function(server) {
+			var main = 's'+server.id,
+			$server = $('#server-' + server.id)
+
+			if (server.status === 1) {
+				$server.addClass('success')
+
+				var video = $server.find('.server-name')
+				if (video.find('video').length === 0)
+					video.prepend('<video width="15" height="15" autoplay loop><source src="img/AlertAnim2.mp4" type="video/mp4"></video>').find('video')[0].play()
+
+				$server.find('.type').text(typeData[server.alert.type]+' (+'+server.alert.experience_bonus+'%)')
+				if (server.alert.type === 1) {
+					$('body').remove('#collapse'+server.id).find('p').before('<div id="collapse'+server.id+'" class="collapse"><div class="container"><div class="progress"><div class="progress-bar progress-bar-danger" style="width:'+Math.round(server.alert.faction_tr)+'%"></div><div class="progress-bar progress-bar-info" style="width:'+Math.round(server.alert.faction_nc)+'%"></div><div class="progress-bar progress-bar-purple" style="width:'+Math.ceil(server.alert.faction_vs)+'%"></div></div></div></div>')
+					$('.collapse').collapse({toggle: false})
+					$server.find('.server-name > button').removeAttr('disabled')
+				} else {
+					$server.find('.server-name > button').attr('disabled', true)
+				}
+				$server.find('.continent').text(zoneData[server.alert.zone])
+				$server.find('.remaining').removeClass('inactive')
+
+				this.updateTime(server)
+			} else {
+				$server.removeClass()
+				$server.find('.server-name video').remove()
+				$server.find('.server-name > button').attr('disabled', true)
+				$server.find('.type').text('')
+				$server.find('.continent').text('')
+				$server.find('.remaining').addClass('inactive')
+			}
+
+			if (typeof server.status === 'string') {
+				$server.find('.remaining').html(server.status.charAt(0).toUpperCase() + server.status.slice(1))
+				if (server.status.search('error') !== -1)
+					$server.addClass('danger')
+
+			}
+		},
+
+		updateTime: function(server) {
+			if (!server) {
+				return $.each(this.servers, function(i, server) {
+					this.updateTime(server)
+				}.bind(this))
+			}
+			var current = Date.now()
+
+			if (server.status === 1) {
+				var date = new Date(server.alert.start - current)
+
+				if (server.alert.type === 1 || server.alert.zone === 0) {
+					date.setUTCHours(date.getUTCHours() + 2)
+				} else {
+					date.setUTCHours(date.getUTCHours() + 1)
+				}
+
+				var h = date.getUTCHours()
+				var m = ('0' + date.getUTCMinutes()).slice(-2)
+				var s = ('0' + date.getUTCSeconds()).slice(-2)
+
+				if (h < 2 || (h + +m + +s) > 0)
+					return $('#server-' + server.id + ' .remaining').html(h + ':' + m + ':' + s)
+				server.status = 'no alert'
+				this.updateTable(server)
+			}
+		},
+
 		updated: function(server) {
-			this.updateAlerts(server)
+			this.updateTable(server)
 		},
 
 		refresh: function(e) {
@@ -39,91 +133,14 @@
 					$(e).removeAttr('disabled')
 				}, 30000)
 			})
-		},
-
-		addHTML: function() {
-			$.each(this.servers, function (index, server) {
-				$('tbody').append('<tr id="server-' + server.id + '"></tr>')
-				$('tr:last').append('<td class="server-name"><button type="button" data-toggle="collapse" data-target="#collapse'+server.id+'" class="btn btn-link">' + server.name + '</button></td>')
-				$('tr:last').append('<td class="remaining"></td>')
-				$('tr:last').append('<td class="type"></td>')
-				$('tr:last').append('<td class="continent"></td>')
-			})
-		},
-
-		updateAlerts: function(server) {
-			var main = 's'+server.id,
-			$server = $('#server-' + server.id)
-			if (server.status == 1) {
-				$server.addClass('success')
-				var video = $server.find('.server-name')
-				if (video.find('video').length === 0)
-					video.prepend('<video width="15" height="15" autoplay loop><source src="img/AlertAnim2.mp4" type="video/mp4"></video>').find('video')[0].play()
-				$server.find('.type').text(server.alert.type+' (+'+server.alert.experience_bonus+'%)')
-				if (server.alert.type === 'Territory') {
-					$('body').remove('#collapse'+server.id).find('p').before('<div id="collapse'+server.id+'" class="collapse"><div class="container"><div class="progress"><div class="progress-bar progress-bar-danger" style="width:'+Math.round(server.alert.faction_tr)+'%"></div><div class="progress-bar progress-bar-info" style="width:'+Math.round(server.alert.faction_nc)+'%"></div><div class="progress-bar progress-bar-purple" style="width:'+Math.ceil(server.alert.faction_vs)+'%"></div></div></div></div>')
-					$('.collapse').collapse({toggle: false})
-					$server.find('.server-name > button').removeAttr('disabled')
-				} else {
-					$server.find('.server-name > button').attr('disabled', true)
-				}
-				$server.find('.continent').text(server.alert.zone)
-				$server.find('.remaining').removeClass('inactive')
-
-				this.updateTime(server)
-				this.intervals[main] = window.setInterval(this.updateTime.bind(this, server), 1000)
-			} else {
-				$server.removeClass()
-				$server.find('.server-name video').remove()
-				$server.find('.server-name > button').attr('disabled', true)
-				$server.find('.type').text('')
-				$server.find('.continent').text('')
-				$server.find('.remaining').addClass('inactive')
-
-				if (this.intervals[main])
-					window.clearInterval(this.intervals[main])
-			}
-
-			if (typeof server.status === 'string') {
-				$server.find('.remaining').html(server.status.charAt(0).toUpperCase() + server.status.slice(1))
-				if (server.status.search('error') !== -1)
-					$server.addClass('danger')
-
-			}
-		},
-
-		updateTime: function(server) {
-			var current = Date.now()
-
-			if (server.status == 1) {
-				var date = new Date(server.alert.start - current)
-
-				if (server.alert.type === 'Territory' || server.alert.zone === 'Global') {
-					date.setUTCHours(date.getUTCHours() + 2)
-				} else {
-					date.setUTCHours(date.getUTCHours() + 1)
-				}
-
-				var h = date.getUTCHours()
-				var m = ('0' + date.getUTCMinutes()).slice(-2)
-				var s = ('0' + date.getUTCSeconds()).slice(-2)
-
-				if (h > 2 || (h + +m + +s) < 0) {
-					server.status = 'no alert'
-					this.updateAlerts(server)
-				} else {
-					$('#server-' + server.id + ' .remaining').html(h + ':' + m + ':' + s)
-				}
-			}
 		}
 	}
 
 	window.App = App
 }(window)
 
-var app
 $.fn.ready(function() {
-	app = new App()
+	App = new App()
 	window.setTimeout(function() {
 		$.each($('video'), function(i, vid) {
 			vid.play()
