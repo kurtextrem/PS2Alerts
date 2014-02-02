ba446e8c1548d14b2efc5455f86f47142fdaa3a4
Jacob Groß <kurtextrem@gmail.com>
2014-02-02 10:06:24 +0100

    Improved server updates from ~6s to ~<1s

---
+++ server/App.class.php
@@ -0,0 +1,169 @@
+<?php
+
+class App {
+	const UPDATE_TIME = 2;
+
+	private $url;
+	private $servers = array(
+	                 	'25' => array('name' => 'Briggs', 'status' => 'API error (U)', 'isOnline' => false, 'alert' => array()),
+	                 	'11' => array('name' => 'Ceres', 'status' => 'API error (U)','isOnline' => false, 'alert' => array()),
+	                 	'13' => array('name' => 'Cobalt', 'status' => 'API error (U)', 'isOnline' => false, 'alert' => array()),
+	                 	'1' => array('name' => 'Connery', 'status' => 'API error (U)', 'isOnline' => false, 'alert' => array()),
+	                 	'17' => array('name' => 'Mattherson', 'status' => 'API error (U)', 'isOnline' => false, 'alert' => array()),
+	                 	'10' => array('name' => 'Miller', 'status' => 'API error (U)', 'isOnline' => false, 'alert' => array()),
+	                 	'18' => array('name' => 'Waterson', 'status' => 'API error (U)', 'isOnline' => false, 'alert' => array()),
+	                	'9' => array('name' => 'Woodman', 'status' => 'API error (U)', 'isOnline' => false, 'alert' => array())
+	);
+
+	private $events = array(
+		array('zone' => 2, 'type' => 1),
+		array('zone'=> 8, 'type' => 1),
+		array('zone'=> 6, 'type' => 1),
+		array('zone'=> 0, 'type' => 2),
+		array('zone'=> 0, 'type' => 3),
+		array('zone'=> 0, 'type' => 4),
+		array('zone'=> 6, 'type' => 2),
+		array('zone'=> 6, 'type' => 3),
+		array('zone'=> 6, 'type' => 4),
+		array('zone'=> 2, 'type' => 2),
+		array('zone'=> 2, 'type' => 3),
+		array('zone'=> 2, 'type' => 4),
+		array('zone'=> 8, 'type' => 2),
+		array('zone'=> 8, 'type' => 4)
+	);
+
+	private $activeEvent = array(
+		'135' => true,
+		'136' => true,
+		'139' => true
+	);
+
+	private $output = array('time' => NOW, 'alertCount' => 0, 'servers' => array());
+
+	function __construct() {
+		if (!isset($_GET['data']))
+			exit($this->setHeader('404'));
+
+		define('NOW', time());
+		define('URL', 'http://census.soe.com/s:'.ID.'/get/ps2:v2/');
+
+		$data = @file_get_contents('cache/data');
+		if (!$data) {
+			$data = '{"time": 0}';
+		}
+		$data = json_decode($data);
+
+		$this->setHeader('json');
+		if (!$this->isOld($data)) {
+			$this->output($data);
+		} else {
+			$this->update();
+		}
+	}
+
+	function setHeader($type) {
+		$header = 'Content-type: application/json';
+		switch ($type) {
+			case 'json':
+				break;
+			case '404':
+				$header = $_SERVER['SERVER_PROTOCOL'].' 404 Not Found';
+				break;
+
+			default:
+				$header = $_SERVER['SERVER_PROTOCOL'].' 404 Not Found';
+				break;
+
+		}
+		header($header);
+	}
+
+	function isOld($data) {
+		return (self::NOW - $data->time <= self::UPDATE_TIME * 60);
+	}
+
+	function output($data, $exit = true) {
+		echo json_encode($data);
+		if ($exit)
+			exit;
+	}
+
+	function update() {
+		$ids = $this->getIDs();
+		$servers = $this->get(URL.'world?c:show=state,world_id&world_id='.$ids);
+		$alerts = $this->sortAlerts($this->get(URL.'world_event?world_id='.$data['id'].'&type=METAGAME'));
+
+		foreach ($servers->world_list as $server) {
+			$data = $this->servers[$server['world_id']];
+			$data['id'] = $server['world_id'];
+
+			if ($server->state === 'online') {
+				$data['isOnline'] = true;
+
+				if (!isset($alerts[$data['id']])) {
+					$data['status'] = 'API error (A)';
+				} else {
+					$data2 = $alerts[$data['id']];
+
+					if (!isset($this->activeEvent[''.$data2->metagame_event_state])) {
+						$data['status'] = 'no alert';
+					} else {
+						$this->output['alertCount']++;
+						$event = $this->events[''.($data2->metagame_event_id - 1)];
+
+						$data['status'] = 1;
+						$data['alert'] = array(
+						                       'start' => $data2->timestamp . '000',
+						                       'type' => $event['type'],
+						                       'zone' => $event['zone'],
+						                       'faction_nc' => $data2->faction_nc,
+						                       'faction_tr' => $data2->faction_tr,
+						                       'faction_vs' => $data2->faction_vs,
+						                       'experience_bonus' => $data2->experience_bonus,
+						                       'facilities' => array()
+						);
+
+						if ($event['type'] > 1) {
+							$file3 = $this->get('http://fishy.sytes.net/ps2territory.php?world='.$data['id'].'&continent='.$event['zone'].'&facility='.$event['type'], true);
+
+							if ($file3) {
+								$data['alert']['facilities'] = $file3['control-list'][0]['facilities'];
+								$data['alert']['faction_vs'] = $file3['control-list'][0]['control-percentage'][1];
+								$data['alert']['faction_nc'] = $file3['control-list'][0]['control-percentage'][2];
+								$data['alert']['faction_tr'] = $file3['control-list'][0]['control-percentage'][3];
+							}
+						}
+					}
+				}
+			} else {
+				$data['status'] = $server['state'];
+			}
+			$this->output['servers'][$server['world_id']] = $data;
+		}
+
+		$this->output($this->output, false);
+		@file_put_contents('cache/data', $json);
+	}
+
+	function getIDs() {
+		$ids = '';
+		foreach ($this->servers as $id) {
+			$ids .= $id.',';
+		}
+		return $ids;
+	}
+
+	function get($url, $array = false) {
+		return json_decode(@file_get_contents($url), $array);
+	}
+
+	function sortAlerts($alerts) {
+		$return = array();
+		foreach ($alerts as $alert) {
+			$return[$alert['world_id']] = $alert;
+		}
+		return $return;
+	}
+}
+
+new App();
