a15429abccdad7f7b46eed0ffae07fcb4aae7b66
Jacob Groß (kurtextrem) <kurtextrem@gmail.com>
2015-12-21 14:32:44 +0100

    update for #24

--- server/App.class.php
+++ server/App.class.php
@@ -1,10 +1,13 @@
 <?php
+error_reporting(ERROR);
 
 class App {
 	/**
 	 * Represents the filename for the cached data.
 	 */
 	const FILE_NAME = 'data.json';
+
+	const DOMAIN = 'http://api.ps2alerts.com/v2/';
 
 	/**
 	 * Checks for the correct update key and updates.
@@ -20,7 +23,7 @@ class App {
 		// get config
 		require_once 'config.inc.php';
 		// set url according to conf
-		define('URL', 'http://api.ps2alerts.com/v2/alert/active?apikey=' . API_KEY);
+		define('URL', self::DOMAIN . 'alert/active?apikey=' . API_KEY);
 
 		if (php_sapi_name() == 'cli' || (isset($_GET['updateKey']) && $_GET['updateKey'] === UPDATE_KEY)) {
 			exit($this->update() ? 'Done' : 'Error');
@@ -37,14 +40,45 @@ class App {
 	 * @date   	2015-12-20
 	 */
 	private function update() {
-		$data = @file_get_contents(URL);
-		$data = @utf8_encode($data);
-		$data = @json_decode($data);
-		$data = array(
+		$data = file_get_contents(URL);
+		$data = $this->completeData($data);
+
+		return @file_put_contents(self::FILE_NAME, json_encode($data));
+	}
+
+	/**
+	 * Requests details for a specific alert.
+	 *
+	 * @author 	Jacob Groß
+	 * @date          	2015-12-21
+	 * @param 	$data
+	 * @return 	array
+	 */
+	private function completeData($data) {
+		foreach($data as &$server) {
+			$map = file_get_contents(self::DOMAIN . 'metrics/map/' . $server['ResultID'] . '/latest');
+			$server['data'] = $this->parseData($map, true)[0];
+		}
+
+		return array(
 			'timestamp' => time() . '000',
 			'data' => $data
 		);
-		return @file_put_contents(self::FILE_NAME, @json_encode($data));
+	}
+
+	/**
+	 * Parses a JSON response.
+	 *
+	 * @author 	Jacob Groß
+	 * @date          	2015-12-21
+	 * @param 	String 	$data
+	 * @param  	bool   	$assoc
+	 * @return 	array / stdClass
+	 */
+	private function parseData($data, $assoc = true) {
+		if (empty($data))
+			$data = '{}';
+		return json_decode(utf8_encode($data), $assoc);
 	}
 
 	/**
