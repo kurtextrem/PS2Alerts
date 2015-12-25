74ed676fc614a8f138773f70571bf85480b668bf
Jacob Groß (kurtextrem) <kurtextrem@gmail.com>
2015-12-25 22:22:08 +0100

    add speedier bind

--- src/js/background.js
+++ src/js/background.js
@@ -341,5 +341,8 @@
 		}
 	}
 
+	// speedier .bind
+	+function(n,t){"use strict";var e=n.bind;t.defineProperty(n,"bind",{value:function(n){var t=this;return 1===arguments.length?function(){return t.apply(n,arguments)}:e.apply(t,arguments)}})}(Function.prototype,Object);
+
 	window.alert = new Alert()
 }(window)
