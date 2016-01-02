e4608f368cecbad36052ada48e5b3134b8733248
Jacob Groß (kurtextrem) <kurtextrem@gmail.com>
2016-01-02 08:50:05 +0100

    Update servers

--- src/js/background.js
+++ src/js/background.js
@@ -21,13 +21,7 @@
 		25: 'Briggs',
 		19: 'Jaeger (Events)',
 		1000: 'Genudine (PS4US)',
-		1001: 'Palos (PS4US)',
-		1002: 'Crux (PS4US)',
-		1003: 'Searhus (PS4US)',
-		1004: 'Xelas (PS4US)',
-		2000: 'Ceres (PS4EU)',
-		2001: 'Lithcorp (PS4EU)',
-		2002: 'Rashnu (PS4EU)'
+		2000: 'Ceres (PS4EU)'
 	}
 
 	var zoneData = {
